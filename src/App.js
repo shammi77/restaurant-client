b1ccf7504e944b0b87bc0fbe2ba5f2d900399eb6
shammi77 <nusratshammi.vutu@gmail.com>
2024-04-17 22:18:12 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 
 import './App.css';
+import About from './Pages/About/About';
 import Contact from './Pages/Contact/Contact';
 import Home from './Pages/Home/Home';
 import Navbar from './Pages/Shared/Navbar';
-import { Routes, Route, Link } from 'react-router-dom';
+import Menu from './Pages/Menu/Menu';
+
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -11,6 +14,8 @@ function App() {
      <Navbar></Navbar>
      <Routes>
       <Route path="/" element={<Home />}/>
+      <Route path="about" element={<About />}/>
+      <Route path="menu" element={<Menu />}/>
       <Route path="contact" element={<Contact />}/>
      </Routes>
     </div>
